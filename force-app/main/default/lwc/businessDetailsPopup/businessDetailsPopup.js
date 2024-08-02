f87ab9eba7e764ce71d08aeada99f856be931732
niavesper <nia.vesper@gmail.com>
2024-08-02 10:00:21 -0600

    Add comments to JS

--- force-app/main/default/lwc/businessDetailsPopup/businessDetailsPopup.js
+++ force-app/main/default/lwc/businessDetailsPopup/businessDetailsPopup.js
@@ -1,3 +1,29 @@
+/**
+ * @fileoverview This JS is part of the LWC BusinessDetailsPopup. For more context, 
+ * see DTS Salesforce Team Jira issue FORCE-161. This file is grossly overcommented
+ * because I am still learning JS and need the comments to help me understand functionality.
+ * 
+ * This component allows users to:
+ * - Display a popup with detailed information about a selected business.
+ * - Close the popup by clicking outside of it or on the X inside the popup.
+ * - Open Google Maps with the business address in a new tab.
+ * - Open URLs for the business website and social meadia accounts in a new tab.
+ * 
+ * Dependencies:
+ * - LWC filterAccountsbyBusinessCategory
+ *  
+ * Usage:
+ * - This component is used to show business details in a popup when a business name is clicked.
+ * in the FilterAccountsByBusinessCategory component.
+ * 
+ * Limitations and Considerations:
+ * - Ensure that the component is provided with valid business details to display.
+ * 
+ * Modification History:
+ * - @version 1.0: Initial version by Ksenia Choate, 06/14/2024
+ * - @version 1.1: Updated by Ksenia Choate to fix the issue with Google Maps link not working, 07/30/2024
+ */
+
 import { LightningElement, api, track } from 'lwc';
 
 export default class BusinessDetailsPopup extends LightningElement {
